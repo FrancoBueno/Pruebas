9f96e729ddb508942f844780e8248425d311e02f
FrancoBueno <francobueno1527@gmail.com>
2023-10-10 22:38:24 -0300

    Commit 1

---
+++ main.js
@@ -0,0 +1,42 @@
+const video = document.getElementById("ar-video");
+const canvas = document.getElementById("ar-canvas");
+const markerInfo = document.getElementById("marker-info");
+
+const constraints = { video: { facingMode: "environment" } };
+
+navigator.mediaDevices.getUserMedia(constraints)
+  .then(function (stream) {
+    video.srcObject = stream;
+    video.onloadedmetadata = function () {
+      video.play();
+    };
+  })
+  .catch(function (error) {
+    console.error("No se pudo acceder a la cámara: " + error);
+  });
+
+video.addEventListener("play", function () {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  const ctx = canvas.getContext("2d");
+  const markerImage = new Image();
+  markerImage.src = "tu-marcador.png"; // Reemplaza con la ruta de tu imagen de marcador
+
+  function detectMarker() {
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(markerImage, 0, 0, canvas.width, canvas.height);
+    const markerData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    // Realiza aquí la detección de marcadores (por ejemplo, comparando pixeles en markerData)
+
+    if (markerDetectado) {
+      markerInfo.style.display = "block";
+    } else {
+      markerInfo.style.display = "none";
+    }
+
+    requestAnimationFrame(detectMarker);
+  }
+
+  detectMarker();
+});
